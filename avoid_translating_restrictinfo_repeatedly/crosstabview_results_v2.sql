46b571dcb5a82b5688e08aa888bf209a0a280db0
Junwang Zhao <zhjwpku@gmail.com>
2025-05-11 02:12:15 +0000

    add performance gain
    Signed-off-by: Junwang Zhao <zhjwpku@gmail.com>

--- avoid_translating_restrictinfo_repeatedly/crosstabview_results_v2.sql
+++ avoid_translating_restrictinfo_repeatedly/crosstabview_results_v2.sql
@@ -123,3 +123,87 @@ from msmts where code_tag = 'patched'
 group by code_tag, num_parts, num_joins
 order by 1, 2, 3 \crosstabview 2 3 4
 
+select 'planning time PWJ ON DIFF' as DIFF;
+
+with head_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(planning_time_ms)
+avg_pt, stddev(planning_time_ms) stddev_pt
+from msmts where code_tag = 'head'
+group by code_tag, num_parts, num_joins, pwj),
+patched_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(planning_time_ms)
+avg_pt, stddev(planning_time_ms) stddev_pt
+from msmts where code_tag = 'patched'
+group by code_tag, num_parts, num_joins, pwj)
+select num_parts,
+num_joins,
+format('s=%s%% md=%s%% pd=%s%%',
+((m.avg_pt - p.avg_pt)/m.avg_pt * 100)::numeric(6, 2),
+(m.stddev_pt/m.avg_pt * 100)::numeric(6, 2),
+(p.stddev_pt/p.avg_pt * 100)::numeric(6, 2))
+from head_avgs m join patched_avgs p using (num_parts, num_joins,
+pwj) where pwj order by 1, 2, 3 \crosstabview 1 2 3
+
+select 'plan mem used PWJ ON DIFF' as DIFF;
+
+with head_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(plan_mem_used_kb)
+avg_pt, stddev(plan_mem_used_kb) stddev_pt
+from msmts where code_tag = 'head'
+group by code_tag, num_parts, num_joins, pwj),
+patched_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(plan_mem_used_kb)
+avg_pt, stddev(plan_mem_used_kb) stddev_pt
+from msmts where code_tag = 'patched'
+group by code_tag, num_parts, num_joins, pwj)
+select num_parts,
+num_joins,
+format('s=%s%% md=%s%% pd=%s%%',
+((m.avg_pt - p.avg_pt)/m.avg_pt * 100)::numeric(6, 2),
+(m.stddev_pt/m.avg_pt * 100)::numeric(6, 2),
+(p.stddev_pt/p.avg_pt * 100)::numeric(6, 2))
+from head_avgs m join patched_avgs p using (num_parts, num_joins,
+pwj) where pwj order by 1, 2, 3 \crosstabview 1 2 3
+
+select 'planning time PWJ OFF DIFF' as DIFF;
+
+with head_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(planning_time_ms)
+avg_pt, stddev(planning_time_ms) stddev_pt
+from msmts where code_tag = 'head'
+group by code_tag, num_parts, num_joins, pwj),
+patched_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(planning_time_ms)
+avg_pt, stddev(planning_time_ms) stddev_pt
+from msmts where code_tag = 'patched'
+group by code_tag, num_parts, num_joins, pwj)
+select num_parts,
+num_joins,
+format('s=%s%% md=%s%% pd=%s%%',
+((m.avg_pt - p.avg_pt)/m.avg_pt * 100)::numeric(6, 2),
+(m.stddev_pt/m.avg_pt * 100)::numeric(6, 2),
+(p.stddev_pt/p.avg_pt * 100)::numeric(6, 2))
+from head_avgs m join patched_avgs p using (num_parts, num_joins,
+pwj) where not pwj order by 1, 2, 3 \crosstabview 1 2 3
+
+select 'plan mem used PWJ OFF DIFF' as DIFF;
+
+with head_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(plan_mem_used_kb)
+avg_pt, stddev(plan_mem_used_kb) stddev_pt
+from msmts where code_tag = 'head'
+group by code_tag, num_parts, num_joins, pwj),
+patched_avgs as
+(select code_tag, num_parts, num_joins, pwj, avg(plan_mem_used_kb)
+avg_pt, stddev(plan_mem_used_kb) stddev_pt
+from msmts where code_tag = 'patched'
+group by code_tag, num_parts, num_joins, pwj)
+select num_parts,
+num_joins,
+format('s=%s%% md=%s%% pd=%s%%',
+((m.avg_pt - p.avg_pt)/m.avg_pt * 100)::numeric(6, 2),
+(m.stddev_pt/m.avg_pt * 100)::numeric(6, 2),
+(p.stddev_pt/p.avg_pt * 100)::numeric(6, 2))
+from head_avgs m join patched_avgs p using (num_parts, num_joins,
+pwj) where not pwj order by 1, 2, 3 \crosstabview 1 2 3
+
